bf7bf70c713aa2e1d3e45908ce1967b30faa55c4
krispycrem <obananas073@gmail.com>
2024-07-17 23:26:11 +0300

    Add part7

---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+
+    fetchResources()
+  }, [baseUrl])
+
+  const create = async (newObject) => {
+    const response = await axios.post(baseUrl, newObject)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [resources, service]
+}
+
+export default useResource
